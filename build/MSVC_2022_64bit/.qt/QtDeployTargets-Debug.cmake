63fa99a6eb904a3c130689699161c7f572e93f1f
The_Iorick <kotolevskij.vlad@mail.ru>
2026-03-16 22:42:46 +0300

    Initial commit

---
+++ build/MSVC_2022_64bit/.qt/QtDeployTargets-Debug.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_DoidsCAD_FILE C:/Users/kotol/DOIDS/DoidsCAD/build/MSVC_2022_64bit/Debug/DoidsCAD.exe)
+set(__QT_DEPLOY_TARGET_DoidsCAD_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_DoidsCAD_RUNTIME_DLLS C:/Qt/6.9.0/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.9.0/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.9.0/msvc2022_64/bin/Qt6Cored.dll)
